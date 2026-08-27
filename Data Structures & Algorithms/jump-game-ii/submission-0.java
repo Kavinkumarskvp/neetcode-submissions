30f216cc99ebc2c43daad7e0259d7bdd0decbe9a
Kavinkumar S <61503816+Kavinkumarskvp@users.noreply.github.com>
2026-08-27 11:16:56 +0530

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jump(int[] nums) {
+        int l = 0, r = 0;
+        int jumps = 0;
+
+        while (r < nums.length - 1) {
+            int farthest = r;
+            for (int i = l; i <= r; i++) {
+                farthest = Math.max(farthest, nums[i] + i);
+            }
+
+            l = r + 1;
+            r = farthest;
+            jumps++;
+        }
+
+        return jumps;
+    }
+}
